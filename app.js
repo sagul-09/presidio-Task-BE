ac684fc2bc0c451da730912bd0b948fd5d5da98d
sagul <nihal.evil.royal@gmail.com>
2023-11-19 21:54:43 +0530

    added  route instructions in app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+    res.send("Welcome to the Movie API Server <br><br> /api/movie <br> /api/movie/add <br> /api/movie/search <br> /api/movie/filter <br> /api/movie/language <br> /api/movie/:id");
+});
 const db = mongoose.connect(process.env.DB_URL).then(()=>{
     console.log("DB is connected"); 
 }).catch((err)=>{
