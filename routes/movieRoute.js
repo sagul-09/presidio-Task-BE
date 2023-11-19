34251a4c673c23b28501c418a64136acecca409b
sagul <nihal.evil.royal@gmail.com>
2023-11-19 21:50:43 +0530

    test

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -2,11 +2,11 @@ import express from "express";
 import {allMovies, addMovie,getMovie,updateMovie, deleteMovie, filterMovies, searchMovies, languageCount} from "../controller/movieController.js";
 const router = express.Router();
 
-router.route("/").get(allMovies)
+router.route("/").get(allMovies);
 router.route("/add").post(addMovie);
 router.route("/search").get(searchMovies);
 router.route("/filter").get(filterMovies);
-router.route("/language").get(languageCount); //help :"( not working
+router.route("/language").get(languageCount);
 router.route("/:id").get(getMovie).put(updateMovie).delete(deleteMovie);
 
 export default router;
